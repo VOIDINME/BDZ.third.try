77a24c676cf4ac5c8b2106000923391602437341
VOIDINME <Art2009mko@mail.ru>
2024-10-21 20:37:15 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,27 @@
 import java.util.Scanner;
-
 public class Main {
+
+    public static String szht(String input) {
+
+        StringBuilder strbuld = new StringBuilder();
+        int count = 1;
+
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == input.charAt(i - 1)) {
+                count++;
+            } else {
+                strbuld.append(input.charAt(i - 1)).append(count);
+                count = 1;
+            }
+        }
+        strbuld.append(input.charAt(input.length() - 1)).append(count);
+        return strbuld.toString();
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        int reversedNumber = reverseDigits(number);
-        System.out.println(reversedNumber);
-    }
-    public static int reverseDigits(int i) {
-        int reversedNumber = 0;
-        while (i > 0) {
-            int lastDigit = i % 10;
-            // буду писать коменты типо крутой ну и чтобы вы поняли что не списываю короче
-            //чтобы узнать число нам нужно последнее число делить на 10 и остаток это и есть число
-            reversedNumber = reversedNumber * 10 + lastDigit;
-            // Здесь удаляяем последнюю цифру
-            i /= 10;
-        }
-        return reversedNumber;//возвращаем нужное нам значение
+        String input = scanner.nextLine();
+
+        String res = szht(input);
+        System.out.println(res);
     }
 }
